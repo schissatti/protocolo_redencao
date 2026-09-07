18e35da63385f9782cfc44e9d493822def632a7c
Vitin <vitin@example.com>
2026-09-07 14:30:14 -0300

    fix: atualizar pasta do brivanne.com no deploy.sh do aaPanel

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Script de Atualização Automática via GitHub no aaPanel
-cd /www/wwwroot/seudominio.com.br
+if [ -d "/www/wwwroot/brivanne.com" ]; then
+    cd /www/wwwroot/brivanne.com
+elif [ -d "/www/wwwroot/seudominio.com.br" ]; then
+    cd /www/wwwroot/seudominio.com.br
+fi
 git pull origin main
 echo "Site e mídias atualizados com sucesso via GitHub!"
